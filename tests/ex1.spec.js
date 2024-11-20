9ba4e8783c4a1307cac4f8d9111e7dad656d2f96
Almog Hakun <almog7531@gmail.com>
2024-11-20 21:45:05 +0200

    do until ex 3

--- tests/ex1.spec.js
+++ tests/ex1.spec.js
@@ -2,19 +2,33 @@ import { test, expect } from "@playwright/test";
 
 test.describe.only("Exercises", () => {
   test("Exercise class", async ({ page }) => {
-    await page.goto(
-      "https://www.google.com/search?q=playwright&sca_esv=581353aac19f9e2d&source=hp&ei=RHk8Z4zpFPn-p84P_vyxkQQ&iflsig=AL9hbdgAAAAAZzyHVL4dYI9iWWjamvrQCWogsDni3dnP&ved=0ahUKEwiMm4yLqOiJAxV5_8kDHX5-LEIQ4dUDCA0&uact=5&oq=&gs_lp=Egdnd3Mtd2l6IgBIAFAAWABwAHgAkAEAmAEAoAEAqgEAuAEDyAEAmAIAoAIAmAMAkgcAoAcA&sclient=gws-wiz"
-    );
-    await page
-      .getByRole("link", { name: "Playwright: Fast and reliable" })
-      .click();
+    await page.goto("https://www.google.com/search?q=playwright&sca_esv=581353aac19f9e2d&source=hp&ei=RHk8Z4zpFPn-p84P_vyxkQQ&iflsig=AL9hbdgAAAAAZzyHVL4dYI9iWWjamvrQCWogsDni3dnP&ved=0ahUKEwiMm4yLqOiJAxV5_8kDHX5-LEIQ4dUDCA0&uact=5&oq=&gs_lp=Egdnd3Mtd2l6IgBIAFAAWABwAHgAkAEAmAEAoAEAqgEAuAEDyAEAmAIAoAIAmAMAkgcAoAcA&sclient=gws-wiz");
+    await page.getByRole("link", { name: "Playwright: Fast and reliable" }).click();
   });
 
-  test("exercise 1", async ({ page }) => {
+  test("Exercise 1", async ({ page }) => {
     await page.goto("https://devexpress.github.io/testcafe/example/");
-    await page.getByTestId("remote-testing-checkbox").check();
-    await page.getByTestId("reusing-js-code-checkbox").check();
-    await page.getByTestId("parallel-testing-checkbox").check();
-    await expect(locator).toBeChecked();
+    const locatorArray = [
+      "remote-testing-checkbox",
+      "reusing-js-code-checkbox",
+      "parallel-testing-checkbox",
+    ];
+    for (let locatorString of locatorArray) {
+      await page.getByTestId(locatorString).check();
+      await expect(page.getByTestId(locatorString)).toBeChecked();
+    }
   });
+
+  test("Exercise 2", async ({ page }) => {
+    await page.goto("https://devexpress.github.io/testcafe/example/");
+    await page.getByTestId("windows-radio").check();
+    await expect(page.getByTestId("windows-radio")).toBeChecked();
+  });
+
+  test("Exercise 3", async ({ page }) => {
+    await page.goto("https://devexpress.github.io/testcafe/example/");
+    await page.getByTestId('preferred-interface-select').selectOption('JavaScript API');
+    await expect(page.getByTestId("preferred-interface-select")).toHaveValue("JavaScript API");
+  });
+
 });
